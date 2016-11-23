3727989c4f281be577311f68af6f2990f752b334
BurpSuite <BurpSuite@iCloud.com>
2016-11-23 13:40:48 +0800

    Fix Name

--- API/Hosts.php
+++ API/Hosts.php
@@ -3,7 +3,7 @@
 # 页面禁止缓存 | UTF-8编码 | 触发下载
 header("cache-control:no-cache,must-revalidate");
 header("Content-Type:text/html;charset=UTF-8");
-header('Content-Disposition: attachment; filename='.'Surge.Conf');
+header('Content-Disposition: attachment; filename='.'Hosts.Conf');
 
 # 默认模块API托管在Github[GithubUserContent] | 模块数组 | 请求模块禁止缓存
 $ModuleAPI    = "https://raw.githubusercontent.com/BurpSuite/CloudGate-RuleList/master/Rule/";
